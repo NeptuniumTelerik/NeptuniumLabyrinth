a5ce518828a3687ae4336a863a850dba9a92983f
Arislan <arislan87@gmail.com>
2013-05-15 04:55:59 +0300

    Modified Interfaces.

--- Labyrinth/IPlayer.cs
+++ Labyrinth/IPlayer.cs
@@ -7,6 +7,7 @@ namespace Labyrinth
 {
     public interface IPlayer
     {
+        char PlayerSymbol { get; }
         int RowPosition { get; set; }
         int ColPosition { get; set; }
         uint MoveCounter { get; set; }
